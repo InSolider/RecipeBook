fc45fe43d3d5642bb2655ee60c9d13687dac0fdd
MaksymKimachenko <maximkimachenko@gmail.com>
2023-01-24 23:22:57 +0200

    The second day of development.

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Recipe
 
-# Create your views here.
+# Відображення рецептів
+
+class RecipeView(View):
+    def get(self, request):
+        recipes = Recipe.objects.all()
+        return render(request, 'recipes/index.html', {'recipes_list': recipes})
