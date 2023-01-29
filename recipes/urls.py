6d9f17a7ebacd0d17bf2cb700e01af5279c3fabc
MaksymKimachenko <maximkimachenko@gmail.com>
2023-01-30 00:40:09 +0200

    The fifth day of development.

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RecipeView.as_view()),
+    path('', views.RecipeView.as_view(), name='home'),
     path('<int:pk>/', views.RecipeDetail.as_view()),
 ]
